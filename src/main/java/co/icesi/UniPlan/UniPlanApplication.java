9e6d0a62c1c70b5d53d81086d8a9cfa2d20e6b6c
andresMC21 <andrescmm0306@gmail.com>
2026-05-11 09:36:18 -0500

    First project directories created for UniPlan implementation

---
+++ src/main/java/co/icesi/UniPlan/UniPlanApplication.java
@@ -0,0 +1,13 @@
+package co.icesi.UniPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/icesi/UniPlan/UniPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package co.icesi.UniPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
